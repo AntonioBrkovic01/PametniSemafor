4286e05e125e25761b9947b80e66320bf217005d
Antonio Brković <abrkovic06@gmail.com>
2026-01-08 20:04:54 +0100

    Vozila drugi dio

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import time
 import os
+import random
 from environment.grid import TrafficGrid
 from agents.vehicle import Vehicle
 import config
@@ -8,24 +9,38 @@ def clear_screen():
     os.system('cls' if os.name == 'nt' else 'clear')
 
 def main():
-    cesta = TrafficGrid()
-    auto = Vehicle(vehicle_id=1, x=0, y=10)
+    WIDTH = 20
+    HEIGHT = 20
+    vozila = []
+    id_counter = 1
 
     print("Pocinje simulacija")
     time.sleep(1)
 
-    for korak in range(15):
+    for korak in range(50):
         clear_screen()
-
         cesta = TrafficGrid()
-        cesta.update_position(auto.x, auto.y, auto.symbol)
 
-        print(f"Korak: {korak +1}")
+        if random.random() < 0.3:
+            if random.choice(['horizontal', 'vertical']) == 'horizontal':
+                novi_auto = Vehicle(id_counter, x=0, y=10, dx=1, dy=0)
+                novi_auto.symbol = ">"
+            else:
+                novi_auto = Vehicle(id_counter, x=10, y=0, dx=0, dy=1)
+                novi_auto.symbol = "v"
+            vozila.append(novi_auto)
+            id_counter += 1
+            
+        vozila = [v for v in vozila if v.is_on_road(WIDTH, HEIGHT)]
+
+        for auto in vozila:
+            cesta.update_position(auto.x, auto.y, auto.symbol)
+            auto.move()
+        
+        print(f"Korak: {korak + 1} | Broj vozila: {len(vozila)}")
         cesta.display()
-
-        auto.move()
-
-        time.sleep(0.5)
+        
+        time.sleep(0.3)
 
     print("Kraj")
 
